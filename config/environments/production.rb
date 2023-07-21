dc527e6f5973cff7b4b2f08fd6e085909b3bc0ca
Zach <zach@zachconley.com>
2023-07-21 15:23:10 -0400

    edit prod host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "issuelist.app"
+  # config.hosts << "issuelist.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
